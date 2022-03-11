45ff304c8cb2b6d202af2ebf7029634156b7be2c
huffmanks <55319887+huffmanks@users.noreply.github.com>
2022-03-11 16:09:50 -0500

    slider

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const prevButton = document.querySelector('.prev .arrow')
 const nextButton = document.querySelector('.next .arrow')
 
 function loadImages() {
-    imageEls.map((img, i) => {
+    imageEls.forEach((img, i) => {
+        // console.log('beg', i)
         img.src = images[i]
+        // const test = setTimeout((img.src = images[i]), 5000)
+        // console.log('inbet', i)
+        // setTimeout(test, 5000)
+        // console.log('end', i)
+        // clearInterval(test)
     })
+    // imageEls.map((img, i) => {
+    //     console.log('beg')
+    //     img.src = images[i]
+    //     const test = setInterval((img.src = images[i]), [3000])
+    //     console.log('end')
+    //     clearInterval(test)
+    // })
 }
 
 function prevImage() {
@@ -25,3 +38,19 @@ function nextImage() {
 
 prevButton.addEventListener('click', prevImage)
 nextButton.addEventListener('click', nextImage)
+
+const mainImage = document.querySelector('#mainImage')
+
+const observer = new MutationObserver((changes) => {
+    changes.forEach((change) => {
+        if (change.attributeName.includes('src')) {
+            let s = mainImage.style
+            s.opacity = 0
+            ;(function fade() {
+                ;(s.opacity -= 0.1) >= 1 ? (s.opacity = 1) : setTimeout(fade, 40)
+            })()
+        }
+    })
+})
+
+observer.observe(mainImage, { attributes: true })
